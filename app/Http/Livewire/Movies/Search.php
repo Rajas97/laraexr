8200a54cf5cfc2a761fb8a5fdd20762db72021d3
Rajas97 <rajasdevelopers@gmail.com>
2021-10-11 01:19:46 +0300

    added OMDb API intergration

--- app/Http/Livewire/Movies/Search.php
+++ app/Http/Livewire/Movies/Search.php
@@ -2,10 +2,35 @@
 
 namespace App\Http\Livewire\Movies;
 
+use Illuminate\Support\Facades\Http;
 use Livewire\Component;
 
 class Search extends Component
 {
+    public $q       = '';
+    public $movies;
+    public function boot()
+    {
+        $this->searchService();
+    }
+
+    public function search()
+    {
+        return $this->searchService();
+    }
+
+    public function searchService()
+    {
+        if(trim($this->q) != '')
+        {
+            $response = Http::get(config('movies.url')."&t=".trim($this->q));
+            if($response->successful())
+            {
+                $this->movies   =  $response->json();
+            }
+        }
+    }
+
     public function render()
     {
         return view('livewire.movies.search');
