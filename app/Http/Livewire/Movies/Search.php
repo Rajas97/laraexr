4808de094d46aa8f1fe6fbc922691073f185e02a
Rajas97 <rajasdevelopers@gmail.com>
2021-10-10 23:41:48 +0300

    added Laravel-Livewire movies search componet

---
+++ app/Http/Livewire/Movies/Search.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Movies;
+
+use Livewire\Component;
+
+class Search extends Component
+{
+    public function render()
+    {
+        return view('livewire.movies.search');
+    }
+}
